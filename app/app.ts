d9bbec92c7a2dfe3e1722b9d9f21dfdfaf47fb2e
David Halewood <davidnhalewood@hotmail.com>
2019-09-14 18:14:39 +0100

    Add error handling

--- app/app.ts
+++ app/app.ts
@@ -3,10 +3,22 @@ import http from 'http'
 import { applyMiddleware, applyRoutes } from "./utils";
 import routes from "./services";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", error => {
+  console.log(error)
+  process.exit(1)
+})
+
+process.on("unhandledRejection", error => {
+  console.log(error)
+  process.exit(1)
+})
 
 const router: express.Application = express()
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
 const server: http.Server = http.createServer(router)
 
 const { PORT = 3000 } = process.env
