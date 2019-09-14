ab0713b41e7b3789235bbe0037c288be64167ac2
David Halewood <davidnhalewood@hotmail.com>
2019-09-14 15:55:03 +0100

    :art: Add eslint and apply

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import http from 'http'
 
-const router: express.Application = express();
-const server: http.Server = http.createServer(router);
-const { PORT = 3000 } = process.env;
+const router: express.Application = express()
+const server: http.Server = http.createServer(router)
+const { PORT = 3000 } = process.env
 
 server.listen(3000, function () {
-  console.log(`Server is running http://localhost:${PORT}...`);
-});
+  console.log(`Server is running http://localhost:${PORT}...`)
+})
